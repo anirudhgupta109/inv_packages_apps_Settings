ezio84/abc-n_packages_apps_Settings

DeviceInfoSettings: Add back ABC version
Change-Id: Ie875701d39e88d27ddc3565e9fd0cf7f475d5639

src/com/android/settings/deviceinfo/ROMVersionPreferenceController.java
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2017 The halogenOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.deviceinfo;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.text.TextUtils;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceController;
+
+public class ROMVersionPreferenceController extends PreferenceController {
+
+    private static final String PROPERTY_ABC_VERSION = "ro.abc.version";
+    private static final String KEY_ABC_VERSION = "modversion";
+
+    public ROMVersionPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return !TextUtils.isEmpty(SystemProperties.get(PROPERTY_ABC_VERSION));
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_ABC_VERSION;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        final Preference pref = screen.findPreference(KEY_ABC_VERSION);
+        if (pref == null) return;
+        String version = SystemProperties.get(PROPERTY_ABC_VERSION);
+        pref.setSummary(version);
+    }
+}
+

anirudhgupta109/inv_packages_apps_Settings

src/com/android/settings/deviceinfo/ROMVersionPreferenceController.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2017 The halogenOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.deviceinfo;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.text.TextUtils;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+public class ROMVersionPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin {
+
+    private static final String PROPERTY_INVICTRIX_VERSION = "ro.invictrix.version";
+    private static final String KEY_INVICTRIX_VERSION = "modversion";
+
+    public ROMVersionPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return !TextUtils.isEmpty(SystemProperties.get(PROPERTY_INVICTRIX_VERSION));
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_INVICTRIX_VERSION;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        final Preference pref = screen.findPreference(KEY_INVICTRIX_VERSION);
+        if (pref == null) return;
+        String version = SystemProperties.get(PROPERTY_INVICTRIX_VERSION);
+        pref.setSummary(version);
+    }
+}
+