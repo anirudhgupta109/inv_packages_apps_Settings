0b5d8168a643336861170630e6a473fbb5de6dbd
maxwen <max.weninger@gmail.com>
2018-04-14 00:09:09 +0200

    Add progress for font apply [2/2]
    - Remove logging
    - Change string to something that sounds better
    
    Change-Id: I12312f3a7859a4244387f5183a3d938bffb5b83f

--- src/com/android/settings/display/FontPickerPreferenceController.java
+++ src/com/android/settings/display/FontPickerPreferenceController.java
@@ -91,4 +91,8 @@ public class FontPickerPreferenceController extends AbstractPreferenceController
             return FontInfo.getDefaultFontInfo();
         }
     }
+
+    public void stopProgress() {
+        mFontPreference.stopProgress();
+    }
 }
